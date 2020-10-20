26155b459d2c287134100bc701e8440b7618bc1b
GabrialStanko15 <gabrial.stanko@patelhs.org>
2020-10-20 14:01:16 +0000

    I added the if else statements for my age

--- Main.java
+++ Main.java
@@ -2,5 +2,27 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("Awesome job!");
+
+
+    int age = 15;
+    
+    
+    if(age == 15)
+    { 
+      System.out.println ("You are 15 years old.");
+    }else{
+      System.out.println ("You are not 15 years old.");
+
+      int myAge = 15;
+      int myBirthday = 1605;
+      int todyasDate = 1020;
+
+      if(myBirthday == 1605)
+      { 
+      System.out.println("I am " + myAge + "years old.");
+      }else{
+        System.out.println("I am still" + myAge + " years old.");
+      }
+    }  
   }
 }
